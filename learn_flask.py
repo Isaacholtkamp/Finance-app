9cb5fa6c72818b675c86f566bcaaeb21d6d2e915
isaacholtkamp <isaacholtkamp@gmail.com>
2019-02-14 13:09:21 -0600

    setting variables from route

--- learn_flask.py
+++ learn_flask.py
@@ -1,7 +1,11 @@
-from flask import flask
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return "Hello, world")
+	return "Hello, world"
+	
+@app.route("/<string:name>")
+def hello(name):
+	return f"Hello, {name}"
